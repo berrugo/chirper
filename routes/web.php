17e5d1831ad1d84a3f55c7fd7b30556e7b129204
Andres Berdugo <berrugo@gmail.com>
2023-09-09 16:36:44 -0500

    Chirps ready

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/chirps', [ChirpController::class, 'index'])->middleware(['auth', 'verified'])->name('chirps');
+
+require __DIR__ . '/auth.php';
